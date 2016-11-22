1a06162570cdfbdc5c857350a8523504d4206797
Ahuv Yearim <ahuvi@Optimus.local>
2016-11-23 01:10:23 +0200

    moved Gruntfile to root-dir

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,31 +1,30 @@
 module.exports = function (grunt) {
-	// load grunt plug-ins
-	grunt.loadNpmTasks('grunt-angular-templates');
-	grunt.loadNpmTasks('grunt-contrib-connect');
-	grunt.loadNpmTasks('grunt-contrib-watch');
-	grunt.loadNpmTasks('grunt-contrib-compass');
-	grunt.loadNpmTasks('grunt-contrib-copy');
-	grunt.loadNpmTasks('grunt-contrib-clean');
-	grunt.loadNpmTasks('grunt-middleware-proxy');
-
+	
 	// configure individual actions/tasks
 	grunt.initConfig({
+		
+		// global variables for the project
+		projVars: {
+			appDir: 'FE/app',
+			cordovaDir: 'FE/cordova'
+		},
+
 		clean: {
-			cordova: ['cordova/www/*']
+			cordova: ['<%= projVars.cordovaDir %>/www/*']
 		},
 		compass: {
 			compile: {
 				options: {
-					sassDir: 'app/styles',
-					cssDir: 'app/css',
-					specify: 'app/styles/**/*.scss'
+					sassDir: '<%= projVars.appDir %>/styles',
+					cssDir: '<%= projVars.appDir %>/css',
+					specify: '<%= projVars.appDir %>/styles/**/*.scss'
 				}
 			}
 		},
 		connect: {
 			server: {
 				options: {
-					base: 'app',
+					base: '<%= projVars.appDir %>',
 					hostname: 'localhost',
 					keepalive: false,
 					livereload: 35729,
@@ -61,57 +60,30 @@ module.exports = function (grunt) {
 				files: [
 					{
 						expand: true,
-						cwd: 'app',
+						cwd: '<%= projVars.appDir %>',
 						src: [
 							'js/**/*.js',
 							'css/**/*.css',
 							'assets/**/*',
-							'index.html'
+							'index.html',
+							'manifest.json'
 						],
-						dest: 'cordova/www/'
+						dest: '<%= projVars.cordovaDir %>/www/'
 					}
 				]
 			}
 		},
 		ngtemplates: {
 			dev: {
-				cwd: 'app',
+				cwd: '<%= projVars.appDir %>',
 				src: 'views/**/*.html',
-				dest: 'app/js/templates/templates.js',
-				options: {
-					module: 'app'
-				}
+				dest: '<%= projVars.appDir %>/js/templates/templates.js',
+				options: { module: 'app' }
 			},
 			devMinifyHtml: {
-				cwd: 'app',
+				cwd: '<%= projVars.appDir %>',
 				src: 'views/**/*.html',
-				dest: 'app/js/templates/templates.js',
-				options: {
-					module: 'app',
-					htmlmin: {
-						collapseBooleanAttributes: true,
-						collapseWhitespace: true,
-						removeAttributeQuotes: true,
-						removeComments: true, // Only if you don't use comment directives! 
-						removeEmptyAttributes: true,
-						removeRedundantAttributes: true,
-						removeScriptTypeAttributes: true,
-						removeStyleLinkTypeAttributes: true
-					}
-				}
-			},
-			dist: {
-				cwd: 'app',
-				src: 'views/**/*.html',
-				dest: 'dist/js/templates/templates.js',
-				options: {
-					module: 'app'
-				}
-			},
-			distMinifyHtml: {
-				cwd: 'app',
-				src: 'views/**/*.html',
-				dest: 'dist/js/templates/templates.js',
+				dest: '<%= projVars.appDir %>/js/templates/templates.js',
 				options: {
 					module: 'app',
 					htmlmin: {
@@ -129,42 +101,51 @@ module.exports = function (grunt) {
 		},
 		watch: {
 			compass: {
-				files: ['app/styles/**/*.scss'],
-				options: {
-					livereload: true
-				},
-				tasks: ['compass:compile']
+				files: ['<%= projVars.appDir %>/styles/**/*.scss'],
+				tasks: ['compass:compile'],
+				options: { livereload: true }
 			},
 			gruntfile: {
 				files: ['Gruntfile.js'],
-				options: {
-					reload: true
-				},
 				tasks: []
 			},
 			index: {
-				files: ['app/index.html'],
-				options: {
-					livereload: true
-				},
-				tasks: []
+				files: ['<%= projVars.appDir %>/index.html'],
+				tasks: [],
+				options: { livereload: true }
 			},
 			js: {
-				files: ['app/js/**/*.js', '!app/js/templates/*.js'],
-				options: {
-					livereload: true
-				},
-				tasks: []
+				files: [
+					'<%= projVars.appDir %>/js/**/*.js',
+					'!<%= projVars.appDir %>/js/templates/*.js' // to prevent reloading twice
+				],
+				tasks: [],
+				options: { livereload: true }
 			},
 			ngtemplates: {
-				files: ['app/views/**/*.html', 'app/*.html'],
-				options: {
-					livereload: true
-				},
-				tasks: ['ngtemplates:dev']
+				files: ['<%= projVars.appDir %>/views/**/*.html'],
+				tasks: ['ngtemplates:dev'],
+				options: { livereload: true }
 			}
 		}
 	});
+
+//-----------------------------------------------------------------------------
+
+	// load grunt plug-ins
+	grunt.loadNpmTasks('grunt-angular-templates');
+	grunt.loadNpmTasks('grunt-contrib-connect');
+	grunt.loadNpmTasks('grunt-contrib-watch');
+	grunt.loadNpmTasks('grunt-contrib-compass');
+	grunt.loadNpmTasks('grunt-contrib-copy');
+	grunt.loadNpmTasks('grunt-contrib-clean');
+	grunt.loadNpmTasks('grunt-middleware-proxy');
+
+//-----------------------------------------------------------------------------
+
+	/**
+	 * Register tasks
+	 */
 
 	// build the app for cordova
 	grunt.registerTask('build:cordova', [
