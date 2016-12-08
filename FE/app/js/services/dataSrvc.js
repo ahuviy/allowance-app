c38ba42df41df16da1c0ddfad49fb95dc0e954eb
Ahuv Yearim <ahuvi@Optimus.local>
2016-12-09 01:42:26 +0200

    dataSrvc tweeking

--- FE/app/js/services/dataSrvc.js
+++ FE/app/js/services/dataSrvc.js
@@ -3,20 +3,26 @@
 		.module('app')
 		.service('dataSrvc', dataSrvc);
 
-	dataSrvc.$inject = ['$rootScope', 'apiMap', 'mockupSrvc', '_', '$http', '$q', 'ionErrorHandlerSrvc', 'cacheSrvc', 'cacheMap'];
+	dataSrvc.$inject = ['$rootScope', 'apiMap', 'mockupMap', '_', '$http', '$q', 'ionErrorHandlerSrvc', 'cacheSrvc', 'cacheMap'];
 
-	function dataSrvc($rootScope, apiMap, mockupSrvc, _, $http, $q, ionErrorHandlerSrvc, cacheSrvc, cacheMap) {
-		this.api = api;
-		this.getApiUrl = getApiUrl;
+	function dataSrvc($rootScope, apiMap, mockupMap, _, $http, $q, ionErrorHandlerSrvc, cacheSrvc, cacheMap) {
+		// functions to export
+		this.api = api;						// performs an API request
+		this.getApiUrl = getApiUrl;			// gets the URL for a specified API request
+		this.enableMockup = enableMockup;	// sets whether api() will check for mocked responses.
 
 		/////////////////////
 
+		var _mockupEnabled = true;
+
 		/**
-		 * First, attempts to mock a server response via 'mockupSrvc'. If unseccessful, attempts
-		 * to use a previously-cached-response. If unsuccessful, sends a real HTTP request to the
-		 * server. Only supports GET/POST requests.
+		 * 1) Attempts to mock a server response via mockupMap.
+		 * 2) Attempts to use a previously-cached-response.
+		 * 3) Sends a real HTTP request to the server.
+		 * Note: only supports GET/POST requests.
+		 * 
 		 * @param {Object} cfg Contains the options for the API request:
-		 *   type {String} Describes the api action to be taken (see apiMap.js).
+		 *   type {String} Specifies the api action to be taken (see apiMap.js).
 		 *   data {Object} The data to pass in POST requests.
 		 *   urlParams {Object} Parameters to compile the urlTemplate into a url.
 		 *   useCachedRes {Boolean} Attempt to retrieve a previously-cached-response. Defaults to false.
@@ -43,9 +49,11 @@
 			var data = reqObj.noPayload ? null : cfg.data;
 
 			// attempt to get a mock response
-			var responsePromise = mockupSrvc.mockup(url, method, data);
-			if (responsePromise) {
-				return wrapForErrorHandler(responsePromise, cfg);
+			if (_mockupEnabled) {
+				var responsePromise = mockup(url, method, data);
+				if (responsePromise) {
+					return wrapForErrorHandler(responsePromise);
+				}
 			}
 
 			// attempt to retrieve a previously-cached-response
@@ -53,8 +61,8 @@
 				var cacheId = method === 'POST' ? cacheMap.apiPost.id : cacheMap.apiGet.id;
 				var cachedResponse = cacheSrvc.get(cacheId, url);
 				if (cachedResponse) {
-					responsePromise = asyncRespondFromCache(cachedResponse);
-					return wrapForErrorHandler(responsePromise, cfg);
+					responsePromise = asyncWrapper(cachedResponse);
+					return wrapForErrorHandler(responsePromise);
 				}
 			}
 
@@ -66,15 +74,9 @@
 			} else {
 				throw new Error('API requests can only be GET or POST.');
 			}
-			return wrapForErrorHandler(responsePromise, cfg);
+			return wrapForErrorHandler(responsePromise);
 
-			function asyncRespondFromCache(cacheRes) {
-				var defer = $q.defer();
-				defer.resolve(cacheRes);
-				return defer.promise;
-			}
-
-			function wrapForErrorHandler(responsePromise, cfg) {
+			function wrapForErrorHandler(responsePromise) {
 				var defer = $q.defer();
 				responsePromise
 					.then(function (response) {
@@ -100,7 +102,6 @@
 						}
 						return defer.reject(response);
 					});
-
 				return defer.promise;
 			}
 		}
@@ -119,9 +120,60 @@
 		 * Retrieves the URL that is used in a specified API request.
 		 * @param {String} type the type of API request.
 		 * @param {Object} params parameters used to compile a urlTemplate.
+		 * @returns {String}
 		 */
 		function getApiUrl(type, params) {
 			return apiMap[type].url || compileUrl(apiMap[type].urlTemplate, params);
+		}
+
+		/**
+		 * A wrapper that converts data into a promise.
+		 * @param {Any} data
+		 * @returns {Promise}
+		 */
+		function asyncWrapper(data) {
+			var defer = $q.defer();
+			defer.resolve(data);
+			return defer.promise;
+		}
+
+		/**
+		 * Check the mockupMap if there is a mocked response
+		 * available for the specified request.
+		 * @param {String} url the request-URL.
+		 * @param {String} method 'GET' | 'POST'.
+		 * @param {Object} data the data to pass in POST requests.
+		 * @returns {Promise|null} a mocked-response-promise, or null if not found. 
+		 */
+		function mockup(url, method, data) {
+			var defer = $q.defer();
+			if (mockupMap[url] &&
+				mockupMap[url].method === method &&
+				angular.equals(mockupMap[url].data, data)) {
+				defer.resolve(mockupMap[url].mockup);
+			} else {
+				return null;
+			}
+			return defer.promise;
+		}
+
+		/**
+		 * Set the default API behavior whether to check for mocked responses.
+		 * @param {Function|Boolean} condition a function that returns a boolean or a boolean.
+		 */
+		function enableMockup(condition) {
+			var retval;
+			if (angular.isFunction(condition)) {
+				retval = condition();
+				if (typeof retval !== 'boolean') {
+					throw new Error('Error in enableMockup: condition evaluates to a non-boolean.');
+				}
+			} else if (typeof condition === 'boolean') {
+				retval = condition;
+			} else {
+				throw new Error('Error in enableMockup: condition is of wrong type.');
+			}
+			_mockupEnabled = retval;
 		}
 	}
 })(angular);
